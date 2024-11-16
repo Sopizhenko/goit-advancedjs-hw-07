aaed1926807340072b878bf4e8c8c47788007e35
Artem <artem.sopizhenko@quick.no>
2024-11-16 19:47:30 +0100

    - all task completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+    private signature: string;
+
+    constructor() {
+        this.signature = Math.random().toString(10);
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    public door: boolean;
+    public key: Key;
+    protected tenants: Person[];
+
+    constructor(key: Key) {
+        this.key = key;
+        this.door = false;
+        this.tenants = [];
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if (key === this.key) {
+            this.door = true;
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
